19b965ea595d964a8cb0974ec4a362e84c721929
palexa <putsik@mail.ru>
2018-10-02 09:06:34 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToolbarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
