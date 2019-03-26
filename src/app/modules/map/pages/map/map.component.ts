8cd0ea393e0e576e00cb7c5ea72462eb73d2fd2a
palexa <palexei98@gmail.com>
2019-03-26 12:33:38 +0300

    adding aerial map with geoserver shapes integration

--- src/app/modules/map/pages/map/map.component.ts
+++ src/app/modules/map/pages/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
+import TileWMS from 'ol/source/TileWMS'
 
 
 
@@ -20,14 +21,68 @@ export class MapComponent implements OnInit {
       layers: [
         new TileLayer({
           source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            // url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            url: `https://{1-4}.aerial.maps.cit.api.here.com/maptile/2.1/maptile/newest/satellite.day/{z}/{x}/{y}/256/png?app_id=bC3EwJd5PpBZQksByia9&app_code=ZgXJboW6NT-PllF8etor9g`
             // url: ' https://mt1.google.com/vt/lyrs=s&x=%7Bx%7D&y=%7By%7D&z=%7Bz%7D'
           })
+        }),
+        // new TileLayer ({
+        //   // extent: [479792.580417208,5919986.873842877,538430.5024032703,5953276.110803715],
+        //   source: new TileWMS(
+        //     ({
+        //       url: 'http://127.0.0.1:8080/geoserver/main/wms',
+        //       params: {
+        //         'LAYERS': 'main:Kvartal_WGS84N35',
+        //         'TILED': true,
+        //       },
+        //       title: 'SPA'
+        //     })
+        //   ),
+        // }),
+        // new TileLayer ({
+        //   // extent: [479792.580417208,5919986.873842877,538430.5024032703,5953276.110803715],
+        //   source: new TileWMS(
+        //     ({
+        //       url: 'http://127.0.0.1:8080/geoserver/main/wms',
+        //       params: {
+        //         'LAYERS': 'main:Vydel_WGS84N35',
+        //         'TILED': true,
+        //       },
+        //       title: 'SPA'
+        //     })
+        //   ),
+        // }),
+    new TileLayer ({
+      // extent: [479792.580417208,5919986.873842877,538430.5024032703,5953276.110803715],
+      source: new TileWMS(
+        ({
+          url: 'http://127.0.0.1:8080/geoserver/main/wms',
+          params: {
+            'LAYERS': 'main:Gidrografija_WGS84N35',
+            'TILED': true,
+          },
+          title: 'SPA'
         })
+      ),
+    }),
+      new TileLayer ({
+        // extent: [479792.580417208,5919986.873842877,538430.5024032703,5953276.110803715],
+        source: new TileWMS(
+          ({
+            url: 'http://127.0.0.1:8080/geoserver/main/wms',
+            params: {
+              'LAYERS': 'main:Line_WGS84N35',
+              'TILED': true,
+            },
+            title: 'SPA'
+          })
+        ),
+      })
       ],
       view: new View({
-        center: [0, 0],
-        zoom: 2
+        // projection: 'EPSG:32635',
+        center: [3016281,7089075],
+        zoom: 12
       })
     });
   }
